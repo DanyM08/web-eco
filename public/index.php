8309a529be04f1c9cc7ecfee6cc7534670141404
Jorge Castellanos <castellanosgonzalezjorgedaniel@gmail.com>
2026-06-03 10:22:29 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Access-Control-Allow-Origin: https://web-five-sable-74.vercel.app');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
